9eb5ea732836466a2ce1365a2437fb2cfb492da3
Lora Mitova <loramitova9@gmail.com>
2023-01-15 00:47:37 +0200

    Add files via upload

---
+++ Programming-Basics/Programming-Basics-with-Python/1.First-Steps-in-Coding-Lab/04_inches_to_centimeters.py
@@ -0,0 +1,3 @@
+inch = float(input())
+centimeter = inch * 2.54
+print(centimeter)
---
+++ Programming-Basics/Programming-Basics-with-Python/1.First-Steps-in-Coding-Lab/06_concatenate_data.py
@@ -0,0 +1,5 @@
+name = input()
+last_name = input()
+age = int(input())
+city = input()
+print(f"You are {name} {last_name}, a {age}-years old person from {city}.")
