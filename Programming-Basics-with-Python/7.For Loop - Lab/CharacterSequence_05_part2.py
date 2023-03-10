0ec237ad8e7a6fdb0c1baa6aa8761418d33d0a42
Lora Mitova <loramitova9@gmail.com>
2023-03-10 23:01:57 +0200

    Add files via upload

---
+++ Programming-Basics-with-Python/7.For Loop - Lab/CharacterSequence_05_part2.py
@@ -0,0 +1,7 @@
+text = input()
+
+# обходя всички символи в моя текст
+# повтаряме: печатаме символ
+
+for letter in text:
+    print(letter)
---
+++ Programming-Basics-with-Python/7.For Loop - Lab/Demo.py
@@ -0,0 +1,3 @@
+name = "Desi"
+print(name[0]) # първи символ
+print(name[len(name) - 1]) # последния символ
