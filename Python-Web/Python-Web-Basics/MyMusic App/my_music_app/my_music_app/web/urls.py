e52dc480089b696a2e8275986b8a9b3ffbc262bc
Lora Mitova <loramitova9@gmail.com>
2023-06-23 18:41:21 +0300

    correct urls

--- Python-Web/Python-Web-Basics/MyMusic App/my_music_app/my_music_app/web/urls.py
+++ Python-Web/Python-Web-Basics/MyMusic App/my_music_app/my_music_app/web/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
         path('add/', add_album, name='add album'),
         path('details/<int:pk>/', details_album, name='details album'),
         path('edit/<int:pk>/', edit_album, name='edit album'),
-        path(' delete/<int:pk>/', delete_album, name='delete album'),
+        path('delete/<int:pk>/', delete_album, name='delete album'),
 
     ])),
     path('profile/', include([
