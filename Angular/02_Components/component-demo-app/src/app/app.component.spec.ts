50004f53596ee375aa462d73283a68af1e8eb036
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-02-12 10:51:59 +0200

    Add files via upload

--- Angular/02_Components/component-demo-app/src/app/app.component.spec.ts
+++ Angular/02_Components/component-demo-app/src/app/app.component.spec.ts
@@ -1,10 +1,16 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent
+      ],
     }).compileComponents();
   });
 
@@ -14,7 +20,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'component-demo-app' title`, () => {
+  it(`should have as title 'component-demo-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('component-demo-app');
