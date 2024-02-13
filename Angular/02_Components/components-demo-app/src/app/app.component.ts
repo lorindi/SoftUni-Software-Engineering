3000f3bc2b44608f71249c7ac599020633588ba1
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-02-13 18:07:29 +0200

    Update component-demo-app

--- Angular/02_Components/components-demo-app/src/app/app.component.ts
+++ Angular/02_Components/components-demo-app/src/app/app.component.ts
@@ -7,5 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'title from AppComponent';
-  users = [{name: 'Lora'}, {name: 'Ivan'}]
+  // users = [{name: 'Lora'}, {name: 'Ivan'}]
+
+  users = [
+    { name: 'Mitko', age: 22, lastName: true },
+    { name: 'Mitko', age: 23, lastName: true },
+    { name: 'Mitko', age: 24, lastName: true },
+    { name: 'Mitko', age: 25, lastName: true },
+    { name: 'Mitko', age: 26, lastName: true },
+  ];
+
+  onOutputFromChild() {
+    console.log('onOutputFromChild');
+
+    
+  }
 }
--- Angular/02_Components/components-demo-app/src/app/navigation/navigation.component.ts
+++ Angular/02_Components/components-demo-app/src/app/navigation/navigation.component.ts
@@ -1,29 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.css',
 })
-export class NavigationComponent {
+export class NavigationComponent implements OnInit, OnChanges {
+  @Input('title') titleFromApp: string = '';
+  @Input('activeUsers') activeUsers: {name: string; age: number; lastName: boolean} [] = [];
+  @Output() onChildOutput = new EventEmitter<boolean>();
+  
   // title = 'hide';
   // toggle = false;
   isActive = false;
 
   inputValue = 'Hello'
 
-  activeUsers = [
-    { name: 'Mitko', age: 22 },
-    { name: 'Mitko', age: 23 },
-    { name: 'Mitko', age: 24 },
-    { name: 'Mitko', age: 25 },
-    { name: 'Mitko', age: 26 },
-  ];
+  ngOnInit(){
+    console.log('navigation -> init');
+    
+  }
+  ngAfterViewInit(){
+    console.log('navigation -> After View Init');
+    
+  }
+  ngOnChanges(changes: SimpleChanges){
+    console.log(changes);
+    
+  }
+ 
 
   handleClick(event: Event, ...args: number[] ): void {
     console.log('clicked!');
     this.isActive = !this.isActive;
-
+    this.onChildOutput.emit(this.isActive)
     // this.toggle = !this.toggle;
     // console.log(this.toggle);
   }
