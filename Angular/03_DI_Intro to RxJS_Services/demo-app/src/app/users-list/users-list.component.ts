ac4f1229873435bc536f0618ae48db65f7f043eb
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-02-23 21:05:11 +0200

    Creating a users list  component

---
+++ Angular/03_DI_Intro to RxJS_Services/demo-app/src/app/users-list/users-list.component.ts
@@ -0,0 +1,26 @@
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  Input,
+} from '@angular/core';
+import { User } from 'src/types/user';
+
+@Component({
+  selector: 'app-users-list',
+  templateUrl: './users-list.component.html',
+  styleUrls: ['./users-list.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class UsersListComponent {
+  @Input() users: User[] = [];
+
+  constructor(private cd: ChangeDetectorRef) {}
+
+  ngOnChanges() {
+    console.log('Invoked from ngOnChanges!');
+  }
+  refresh() {
+    this.cd.detectChanges();
+  }
+}
