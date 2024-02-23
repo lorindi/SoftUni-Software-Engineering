bcc16262510dcbe0dbab991f9970e6e0491ff425
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-02-23 21:04:54 +0200

    Creating a users list item component

---
+++ Angular/03_DI_Intro to RxJS_Services/demo-app/src/app/users-list-item/users-list-item.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+import { User } from 'src/types/user';
+
+@Component({
+  selector: 'app-users-list-item',
+  templateUrl: './users-list-item.component.html',
+  styleUrls: ['./users-list-item.component.css'],
+})
+export class UsersListItemComponent {
+  @Input() user: User | undefined = undefined;
+}
