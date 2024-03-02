1108c7081b586820cb805957bbdf6322c48d4168
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-03-02 11:30:11 +0200

    Add files via upload

--- Angular/04_Workshop Components/workshop-components/src/app/core/header/header.component.ts
+++ Angular/04_Workshop Components/workshop-components/src/app/core/header/header.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-
+  isLoggedIn = false;
 }
--- Angular/04_Workshop Components/workshop-components/src/app/themes-list/themes-list.component.ts
+++ Angular/04_Workshop Components/workshop-components/src/app/themes-list/themes-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../api.service';
+import { Theme } from '../types/theme';
 
 @Component({
   selector: 'app-themes-list',
@@ -7,11 +8,12 @@ import { ApiService } from '../api.service';
   styleUrls: ['./themes-list.component.css'],
 })
 export class ThemesListComponent implements OnInit {
+  themes: Theme[] = [];
   constructor(private api: ApiService) {}
   ngOnInit(): void {
     this.api.getThemes().subscribe((themes) => {
       console.log(themes);
-      
-    })
+      this.themes = themes;
+    });
   }
 }
