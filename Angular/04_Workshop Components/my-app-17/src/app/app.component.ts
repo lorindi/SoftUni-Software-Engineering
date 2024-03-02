53496c2e7c59ddc84e68752951c727f3d87d6422
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-03-02 16:56:00 +0200

    Creating users and view with 17 version

--- Angular/04_Workshop Components/my-app-17/src/app/app.component.ts
+++ Angular/04_Workshop Components/my-app-17/src/app/app.component.ts
@@ -6,8 +6,15 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'my-app-17';
+
+  tasks = [
+    { id: 1, name: 'Finish homework', status: 'done' },
+    { id: 2, name: 'Throw out the trash', status: 'todo' },
+    { id: 3, name: 'Change the cars oil', status: 'inProgress' },
+    { id: 4, name: 'Watch a SoftUni lecture', status: 'inProgress' },
+  ];
 }
