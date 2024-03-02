978d5ff928a00affed5547142e00d5075135f2ef
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-03-02 17:14:44 +0200

    Creating component- home-test

---
+++ Angular/04_Workshop Components/my-app-17/src/app/home-test/home-test.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-test',
+  standalone: true,
+  imports: [],
+  templateUrl: './home-test.component.html',
+  styleUrl: './home-test.component.css'
+})
+export class HomeTestComponent {
+
+}
