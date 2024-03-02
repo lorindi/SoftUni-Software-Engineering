edc0758c18c277e292d8a247fe4c2aa1068c5157
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-03-02 17:38:31 +0200

    Creating defer view

--- Angular/04_Workshop Components/my-app-17/src/app/app.component.ts
+++ Angular/04_Workshop Components/my-app-17/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeTestComponent } from './home-test/home-test.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, HomeTestComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
