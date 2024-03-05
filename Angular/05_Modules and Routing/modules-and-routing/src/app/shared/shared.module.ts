2f71dfc317e0ae7e1e6f5869ef06ba62ce92d158
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-03-05 18:35:20 +0200

    Update shared.module.ts

--- Angular/05_Modules and Routing/modules-and-routing/src/app/shared/shared.module.ts
+++ Angular/05_Modules and Routing/modules-and-routing/src/app/shared/shared.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SpinnerComponent } from './spinner/spinner.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SpinnerComponent
+  ],
   imports: [
     CommonModule
   ]
