234fdd6c35f4b64e08c04b2d465992fd78d52c1e
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-03-05 18:31:11 +0200

    Add core module

--- Angular/05_Modules and Routing/modules-and-routing/src/app/app.module.ts
+++ Angular/05_Modules and Routing/modules-and-routing/src/app/app.module.ts
@@ -6,15 +6,9 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
