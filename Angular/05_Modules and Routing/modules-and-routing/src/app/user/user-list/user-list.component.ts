ae5b8d97c0280a85533d17b737b7b85be6e9e412
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-03-05 19:07:27 +0200

    Component reuse

--- Angular/05_Modules and Routing/modules-and-routing/src/app/user/user-list/user-list.component.ts
+++ Angular/05_Modules and Routing/modules-and-routing/src/app/user/user-list/user-list.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./user-list.component.css']
 })
 export class UserListComponent {
+  isLoading = false
+  fetchUsers(){
+    this.isLoading = true;
+    setTimeout(() => {
+      this.isLoading = false
+    },3000)
+  }
 
 }
