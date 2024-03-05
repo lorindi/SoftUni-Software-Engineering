71e05b567963b39f966c9167535e289e9c107d7a
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-03-05 18:49:27 +0200

    Import Shared module

--- Angular/05_Modules and Routing/modules-and-routing/src/app/app.module.ts
+++ Angular/05_Modules and Routing/modules-and-routing/src/app/app.module.ts
@@ -4,12 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SharedModule } from './shared/shared.module';
+// import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, SharedModule, CoreModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    // SharedModule,
+    CoreModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- Angular/05_Modules and Routing/modules-and-routing/src/app/core/core.module.ts
+++ Angular/05_Modules and Routing/modules-and-routing/src/app/core/core.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GlobalLoaderComponent } from './global-loader/global-loader.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 
@@ -9,7 +10,7 @@ import { GlobalLoaderComponent } from './global-loader/global-loader.component';
     GlobalLoaderComponent
   ],
   imports: [
-    CommonModule
+    CommonModule, SharedModule
   ],
   exports: [GlobalLoaderComponent],
 })
