ded3e27e60685b69bfc58cc2c68e76aef5a2ede6
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-03-05 20:22:33 +0200

    Update global-loader

--- Angular/05_Modules and Routing/modules-and-routing/src/app/core/global-loader/global-loader.service.ts
+++ Angular/05_Modules and Routing/modules-and-routing/src/app/core/global-loader/global-loader.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GlobalLoaderService {
+  isHidden: boolean = true;
 
-  constructor() { }
+  showLoader() {
+    this.isHidden = false;
+  }
+  hideLoader() {
+    this.isHidden = true;
+  }
 }
--- Angular/05_Modules and Routing/modules-and-routing/src/app/user/user-list/user-list.component.ts
+++ Angular/05_Modules and Routing/modules-and-routing/src/app/user/user-list/user-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../user.service';
 import { User } from 'src/app/types/user';
+import { GlobalLoaderService } from 'src/app/core/global-loader/global-loader.service';
 
 @Component({
   selector: 'app-user-list',
@@ -11,16 +12,26 @@ export class UserListComponent implements OnInit {
   isLoading = false;
   users: User[] = [];
 
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private globalLoaderService: GlobalLoaderService
+  ) {}
 
   ngOnInit(): void {
     this.fetchUsers();
   }
 
   fetchUsers() {
+    //before fetch
+    this.globalLoaderService.showLoader();
+
+    //start fetching
     this.userService.getUsers().subscribe((users) => {
       console.log(users);
       this.users = users;
+
+      //after fetched
+      this.globalLoaderService.hideLoader();
     });
   }
 
