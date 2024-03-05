978ae3c6c0075af8155ac43c191dd037d8c59215
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-03-05 19:30:05 +0200

    Import HttpClientModule

--- Angular/05_Modules and Routing/modules-and-routing/src/app/user/user.module.ts
+++ Angular/05_Modules and Routing/modules-and-routing/src/app/user/user.module.ts
@@ -2,16 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserListComponent } from './user-list/user-list.component';
 import { SharedModule } from '../shared/shared.module';
-
-
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    UserListComponent
-  ],
-  imports: [
-    CommonModule, SharedModule
-  ],
-  exports: [UserListComponent]
+  declarations: [UserListComponent],
+  imports: [CommonModule, SharedModule, HttpClientModule],
+  exports: [UserListComponent],
 })
-export class UserModule { }
+export class UserModule {}
