da9cd22021a81f2beaee2ad8e5d06b9a0781a4f2
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-03-05 19:35:08 +0200

    Import change

--- Angular/05_Modules and Routing/modules-and-routing/src/app/app.module.ts
+++ Angular/05_Modules and Routing/modules-and-routing/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 // import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core/core.module';
 import { UserModule } from './user/user.module';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [AppComponent],
@@ -17,6 +19,7 @@ import { UserModule } from './user/user.module';
     // SharedModule,
     CoreModule,
     UserModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- Angular/05_Modules and Routing/modules-and-routing/src/app/user/user.module.ts
+++ Angular/05_Modules and Routing/modules-and-routing/src/app/user/user.module.ts
@@ -2,11 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserListComponent } from './user-list/user-list.component';
 import { SharedModule } from '../shared/shared.module';
-import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [UserListComponent],
-  imports: [CommonModule, SharedModule, HttpClientModule],
+  imports: [CommonModule, SharedModule],
   exports: [UserListComponent],
 })
 export class UserModule {}
