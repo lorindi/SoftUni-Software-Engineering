1afca876855e4a50ec127f60267f20eca4e92e49
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-03-05 19:59:37 +0200

    Add files via upload

--- Angular/05_Modules and Routing/modules-and-routing/src/app/user/user-list/user-list.component.ts
+++ Angular/05_Modules and Routing/modules-and-routing/src/app/user/user-list/user-list.component.ts
@@ -1,17 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.css']
+  styleUrls: ['./user-list.component.css'],
 })
-export class UserListComponent {
-  isLoading = false
-  fetchUsers(){
-    this.isLoading = true;
-    setTimeout(() => {
-      this.isLoading = false
-    },3000)
+export class UserListComponent implements OnInit {
+  isLoading = false;
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.fetchUsers();
   }
 
+  fetchUsers() {
+    this.userService.getUsers().subscribe((users) => {
+      console.log(users);
+    });
+  }
+
+  // fetchUsers(){
+  //   this.isLoading = true;
+  //   setTimeout(() => {
+  //     this.isLoading = false
+  //   },3000)
+  // }
 }
--- Angular/05_Modules and Routing/modules-and-routing/src/app/user/user.service.ts
+++ Angular/05_Modules and Routing/modules-and-routing/src/app/user/user.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { User } from '../types/user';
 
 @Injectable({
   providedIn: 'root',
@@ -9,6 +10,6 @@ export class UserService {
   constructor(private http: HttpClient) {}
 
   getUsers() {
-    //
+    return this.http.get<User[]>(this.API)
   }
 }
