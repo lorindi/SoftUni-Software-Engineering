b37b756b908c2fbe60ded0ca44a13a2c57383968
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-03-05 20:08:38 +0200

    Add files via upload

--- Angular/05_Modules and Routing/modules-and-routing/src/app/user/user-list/user-list.component.ts
+++ Angular/05_Modules and Routing/modules-and-routing/src/app/user/user-list/user-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../user.service';
+import { User } from 'src/app/types/user';
 
 @Component({
   selector: 'app-user-list',
@@ -8,6 +9,7 @@ import { UserService } from '../user.service';
 })
 export class UserListComponent implements OnInit {
   isLoading = false;
+  users: User[] = [];
 
   constructor(private userService: UserService) {}
 
@@ -18,6 +20,7 @@ export class UserListComponent implements OnInit {
   fetchUsers() {
     this.userService.getUsers().subscribe((users) => {
       console.log(users);
+      this.users = users;
     });
   }
 
