35f0422d6a935986b54da44a05fb3e618c67e189
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-03-06 22:36:09 +0200

    Create nav.component.ts

---
+++ Angular/05_Modules and Routing/modules-and-routing/src/app/core/nav/nav.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent {
+
+}
