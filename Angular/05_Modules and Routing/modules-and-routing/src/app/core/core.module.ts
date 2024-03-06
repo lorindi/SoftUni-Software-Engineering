e97853668cfde2edb72f304ef12fda4f090eac66
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-03-06 22:35:58 +0200

    Update core.module.ts

--- Angular/05_Modules and Routing/modules-and-routing/src/app/core/core.module.ts
+++ Angular/05_Modules and Routing/modules-and-routing/src/app/core/core.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GlobalLoaderComponent } from './global-loader/global-loader.component';
 import { SharedModule } from '../shared/shared.module';
+import { NavComponent } from './nav/nav.component';
 
 
 
 @NgModule({
   declarations: [
-    GlobalLoaderComponent
+    GlobalLoaderComponent,
+    NavComponent
   ],
   imports: [
     CommonModule, SharedModule
