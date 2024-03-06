f898d4360a0fd4640682f617870cb415ce310360
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-03-06 22:24:13 +0200

    Update roads

--- Angular/05_Modules and Routing/modules-and-routing/src/app/app-routing.module.ts
+++ Angular/05_Modules and Routing/modules-and-routing/src/app/app-routing.module.ts
@@ -4,9 +4,13 @@ import { TodoListComponent } from './todo/todo-list/todo-list.component';
 import { UserListComponent } from './user/user-list/user-list.component';
 
 const routes: Routes = [
-  {path: '', pathMatch: 'full', component: TodoListComponent},
+  // {path: '', pathMatch: 'full', component: TodoListComponent},
+  {path: '', pathMatch: 'full', redirectTo: 'user-list'},
   {path: 'user-list', component: UserListComponent},
   {path: 'todo-list', component: TodoListComponent},
+  {path: 'error-page',},
+  {path: '**', redirectTo: 'error-page'},
+
 
 ];
 
--- Angular/05_Modules and Routing/modules-and-routing/src/app/shared/shared.module.ts
+++ Angular/05_Modules and Routing/modules-and-routing/src/app/shared/shared.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SpinnerComponent } from './spinner/spinner.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
 
 
 
 @NgModule({
   declarations: [
-    SpinnerComponent
+    SpinnerComponent,
+    ErrorPageComponent
   ],
   imports: [
     CommonModule
