2d9ea635eb98ec1cd41727c32c7b58c69fd71eb9
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-03-08 10:32:08 +0200

    Create user-detail.guard.ts

---
+++ Angular/05_Modules and Routing/modules-and-routing/src/app/user/user-detail/user-detail.guard.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class AuthGuard implements CanActivate {
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | boolean
+    | UrlTree
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree> {
+        console.log(state.url);
+        
+    return this.checkIfLoggedIn();
+  }
+  checkIfLoggedIn() {
+    return false;
+  }
+}
