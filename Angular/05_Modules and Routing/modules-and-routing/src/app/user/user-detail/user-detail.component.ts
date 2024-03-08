17908ef44289fdaec0e7cf8192f85ebbb5435174
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-03-08 07:47:54 +0200

    Create user-detail.component.ts

---
+++ Angular/05_Modules and Routing/modules-and-routing/src/app/user/user-detail/user-detail.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-user-detail',
+  templateUrl: './user-detail.component.html',
+  styleUrls: ['./user-detail.component.css'],
+})
+export class UserDetailComponent implements OnInit {
+  constructor(
+    private userService: UserService,
+    private activatedRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe((v) => {
+      const id = v['id'];
+      this.userService.getSingUser(id).subscribe((user) => {
+        console.log(user);
+      });
+    });
+  }
+}
