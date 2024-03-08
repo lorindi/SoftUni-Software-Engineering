002b4122493b6bd3083ec17a692080464433d720
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-03-08 10:32:31 +0200

    Update user.service.ts

--- Angular/05_Modules and Routing/modules-and-routing/src/app/user/user.service.ts
+++ Angular/05_Modules and Routing/modules-and-routing/src/app/user/user.service.ts
@@ -6,16 +6,16 @@ import { User } from '../types/user';
   providedIn: 'root',
 })
 export class UserService {
-  getSingleUser(id: any): User | import("rxjs").Observable<User> | Promise<User> {
-      throw new Error('Method not implemented.');
-  }
+  // getSingleUser(id: any): User | import("rxjs").Observable<User> | Promise<User> {
+  //     throw new Error('Method not implemented.');
+  // }
   private API = 'https://jsonplaceholder.typicode.com/users';
   constructor(private http: HttpClient) {}
 
   getUsers() {
     return this.http.get<User[]>(this.API);
   }
-  getSingUser(id: string) {
+  getSingleUser(id: string) {
     return this.http.get<User>(`${this.API}/${id}`);
   }
 }
