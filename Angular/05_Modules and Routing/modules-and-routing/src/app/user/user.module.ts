2e003eaa1d42b39ec21c66c51fbf424abc3b5b65
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-03-08 07:48:14 +0200

    Update user.module.ts

--- Angular/05_Modules and Routing/modules-and-routing/src/app/user/user.module.ts
+++ Angular/05_Modules and Routing/modules-and-routing/src/app/user/user.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserListComponent } from './user-list/user-list.component';
 import { SharedModule } from '../shared/shared.module';
+import { RouterModule } from '@angular/router';
+import { UserDetailComponent } from './user-detail/user-detail.component';
 
 @NgModule({
-  declarations: [UserListComponent],
-  imports: [CommonModule, SharedModule],
+  declarations: [UserListComponent, UserDetailComponent, UserDetailComponent],
+  imports: [
+    CommonModule,
+    SharedModule,
+    RouterModule.forChild([
+      {
+        path: 'user/list',
+        component: UserListComponent,
+      },
+      { path: 'user/detail/:id', component: UserDetailComponent },
+    ]),
+  ],
   exports: [UserListComponent],
 })
 export class UserModule {}
