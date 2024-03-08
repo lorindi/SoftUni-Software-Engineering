408096391e119b8542850ff88cfe249a78436145
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-03-08 08:35:49 +0200

    Create user-detail.resolver.ts

---
+++ Angular/05_Modules and Routing/modules-and-routing/src/app/user/user-detail/user-detail.resolver.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  Resolve,
+  RouterStateSnapshot,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { User } from 'src/app/types/user';
+import { UserService } from '../user.service';
+
+@Injectable({ providedIn: 'root' })
+export class UserResolve implements Resolve<User> {
+  constructor(private userService: UserService) {}
+
+  resolve(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): User | Observable<User> | Promise<User> {
+    const id = route.params['id'];
+    return this.userService.getSingleUser(id);
+  }
+}
+
