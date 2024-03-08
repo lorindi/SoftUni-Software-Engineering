c6b76df910659a575a144fd7e7d727eef0be40df
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-03-08 07:47:40 +0200

    Update app-routing.module.ts

--- Angular/05_Modules and Routing/modules-and-routing/src/app/app-routing.module.ts
+++ Angular/05_Modules and Routing/modules-and-routing/src/app/app-routing.module.ts
@@ -2,15 +2,24 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TodoListComponent } from './todo/todo-list/todo-list.component';
 import { UserListComponent } from './user/user-list/user-list.component';
-import { ErrorPageComponent } from './shared/error-page/error-page.component';
+// import { ErrorPageComponent } from './shared/error-page/error-page.component';
 
-const routes: Routes = [
-  // {path: '', pathMatch: 'full', component: TodoListComponent},
-  {path: '', pathMatch: 'full', redirectTo: 'user-list'},
-  {path: 'user-list', component: UserListComponent},
+// const routes: Routes = [
+//   // {path: '', pathMatch: 'full', component: TodoListComponent},
+//   {path: '', pathMatch: 'full', redirectTo: 'user-list'},
+//   {path: 'user-list', component: UserListComponent},
+//   // {path: 'user/list', component: UserListComponent},
+
+//   {path: 'todo-list', component: TodoListComponent},
+//   {path: 'error-page', component: ErrorPageComponent},
+//   {path: '**', redirectTo: 'error-page'},
+
+
+// ];
+export const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'user/list'},
   {path: 'todo-list', component: TodoListComponent},
-  {path: 'error-page', component: ErrorPageComponent},
-  {path: '**', redirectTo: 'error-page'},
+
 
 
 ];
