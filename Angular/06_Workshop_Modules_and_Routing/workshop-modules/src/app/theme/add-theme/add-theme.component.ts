3544211d5ec07e9aa4740923a1d48d4c73c5def1
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-03-09 22:25:26 +0200

    Create add-theme.component.ts

---
+++ Angular/06_Workshop_Modules_and_Routing/workshop-modules/src/app/theme/add-theme/add-theme.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-theme',
+  templateUrl: './add-theme.component.html',
+  styleUrls: ['./add-theme.component.css']
+})
+export class AddThemeComponent {
+
+}
