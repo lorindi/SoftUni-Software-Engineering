468a2b7e064dd0b0c4b38e468f4c0661b16f38ab
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-04-01 14:51:57 +0300

    Update index.js

--- JS-Back-End/1.Intro to Node.js/cat-shelter/index.js
+++ JS-Back-End/1.Intro to Node.js/cat-shelter/index.js
@@ -1,49 +1,59 @@
-const http = require('http');
-const { homeTemplate, catTemplate } = require('./views/home/index');
-const siteCss = require('./content/styles/site');
-const addBreedHtml = require('./views/addBreed');
+// const { homeTemplate, catTemplate } = require('./views/home/index');
+// const addBreedHtml = require('./views/addBreed');
+const siteCss = require("./content/styles/site");
+const http = require("http");
+const fs = require("fs/promises");
 
 const cats = [
-    {
-        id: 1,
-        name: 'Navcho',
-        breed: 'Persian',
-        description: 'Very cute cat',
-    },
-    {
-        id: 2,
-        name: 'Mishi',
-        breed: 'Angora',
-        description: 'Fluffy cat',
-    },
-    {
-        id: 3,
-        name: 'Garry',
-        breed: 'Angora',
-        description: 'Fat cat',
-    },
+  {
+    id: 1,
+    name: "Navcho",
+    breed: "Persian",
+    description: "Very cute cat",
+  },
+  {
+    id: 2,
+    name: "Mishi",
+    breed: "Angora",
+    description: "Fluffy cat",
+  },
+  {
+    id: 3,
+    name: "Garry",
+    breed: "Angora",
+    description: "Fat cat",
+  },
 ];
 
 const server = http.createServer(async (req, res) => {
-    console.log(req.url);
+  console.log(req.url);
 
-    if (req.url == '/') {
+  if (req.url == "/") {
+    const homeHtml = await fs.readFile("./views/home/index.html", "utf-8");
+    const catHtml = await fs.readFile("./views/cat.html", "utf-8");
 
-        
-        const catsHtml = cats.map(cat => catTemplate.replace('{{catName}}', cat.name));
-        const homeHtml = homeTemplate.replace('{{cats}}', catsHtml);
+    const catsHtml = cats.map((cat) =>
+      Object.keys(cat).reduce((result, key) => {
+        result = result.replace(`{{${key}}}`, cat[key]);
+        return result;
+      }, catHtml)
+    );
+    const homeHtmlResult = homeHtml.replace("{{cats}}", catsHtml);
 
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write(homeHtml);
-    } else if (req.url == '/styles/site.css') {
-        res.writeHead(200, { 'Content-Type': 'text/css' });
-        res.write(siteCss);
-    } else if (req.url == '/cats/add-breed') {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write(addBreedHtml);
-    }
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write(homeHtmlResult);
+  } else if (req.url == "/styles/site.css") {
+    res.writeHead(200, { "Content-Type": "text/css" });
+    res.write(siteCss);
+  } else if (req.url == "/cats/add-breed") {
+    const addBreadHtml = await fs.readFile("./views/addBreed.html", "utf-8");
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write(addBreadHtml);
+  }
 
-    res.end();
+  res.end();
 });
 
-server.listen(5000, () => console.log('This server is running on port 5000...'));
+server.listen(5000, () =>
+  console.log("This server is running on port 5000...")
+);
