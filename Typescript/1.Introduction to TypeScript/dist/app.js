7c616bf1564b7b4f49a68f30178bce997090cef1
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-04-02 09:33:09 +0300

    Create app.js

---
+++ Typescript/1.Introduction to TypeScript/dist/app.js
@@ -0,0 +1,9 @@
+"use strict";
+const b = 5;
+console.log(b);
+const greeting = "Hello!";
+console.log(greeting);
+// *tsc app.ts
+// *npm install -g typescript (latest stable build)
+// *tsc -v/ tsc --version => Should return a message 'Version'
+// *tsc --init => This command will create a ne tsconfig.json file
