fe4e5aaf301be3c1b357ae02f2ab747e38ed48dc
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-04-02 09:33:05 +0300

    Create app.ts

---
+++ Typescript/1.Introduction to TypeScript/app.ts
@@ -0,0 +1,13 @@
+const b: number = 5;
+console.log(b);
+const greeting: string = "Hello!";
+console.log(greeting);
+
+// *tsc app.ts
+// *npm install -g typescript (latest stable build)
+// *tsc -v/ tsc --version => Should return a message 'Version'
+// *tsc --init => This command will create a ne tsconfig.json file
+
+// * ES6 -> ES5  => Transpile
+// * TypeScript -> JavaScript => Transpile
+// * TypeScript -> C++ => Compile
