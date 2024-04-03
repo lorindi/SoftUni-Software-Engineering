10b9deb8c16f4e70ce14b4ecfb44ab5f69baf5d0
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-04-03 10:26:06 +0300

    Update index.js

--- JS-Back-End/3.ExpressJS and Templating/index.js
+++ JS-Back-End/3.ExpressJS and Templating/index.js
@@ -2,27 +2,55 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
+//Add third party middleware
+// Is there any data in the request post that has been sent, and if there is,
+// it parses it and attaches it to the request body,
+//  and then from the slash url we get request.body.name
+const bodyParser = express.urlencoded({ extended: false });
+app.use(bodyParser);
+// app.use(express.json())
+
+// Every time there is a request first check 
+// if there is no static file with this address in the public folder, 
+// if there is then return the address and terminate the request
+app.use(express.static("public"));
+
 //Add middlewares
 app.use((req, res, next) => {
-  console.log('Middleware1');
-  next()
-})
+  console.log("Middleware1");
+  next();
+});
 
 app.use((req, res, next) => {
   console.log(`Http Request ${req.method}: ${req.path}`);
-  next()
+  next();
 });
 
 //Route based middleware / Partial route middleware
-app.use('/cats', (req, res, next) => {
-  console.log('Cats middleware');
-  next()
-})
+app.use("/cats", (req, res, next) => {
+  console.log("Cats middleware");
+  next();
+});
+
+//Route specific middleware
+const specificMiddleware = (req, res, next) => {
+  console.log("Specific middleware only for this route");
+  next();
+};
+
+app.get("/specific", specificMiddleware, (req, res) => {
+  res.send("Some specific route with middleware");
+});
 
 //Express Router  / Actions
 app.get("/", (req, res) => {
   res.status(200).send(`Hello from Express!`);
 });
+
+//Don't do this at home!
+// app.get("/css/style.css", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "public/css/style.css"));
+// });
 
 //Web Action:(req, res) => {
 //   res.send("This page contains cats :)");
@@ -31,10 +59,34 @@ app.get("/", (req, res) => {
 // http method + path + handler = Ednpoint
 
 app.get("/cats", (req, res) => {
-  res.send("This page contains cats :)");
+  res.send(`
+  <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/style.css">
+    <title>Document</title>
+</head>
+<body>
+  <form method="post" action="">
+    <div>
+        <label htmlFor="name">Name</label>
+        <input type="text" name="name" id="name"/>
+    </div>
+    <div>
+        <label htmlFor="age">Age</label>
+        <input type="number" id="age" name="age"/>
+    </div>
+    <input type="submit" value="create"/>
+  </form>
+</body>
+</html>
+  `);
 });
 
 app.post("/cats", (req, res) => {
+  console.log(req.body.name);
   res.status(201).send("Cat has been created!");
 });
 
