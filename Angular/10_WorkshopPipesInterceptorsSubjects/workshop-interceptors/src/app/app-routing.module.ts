f40adc99cb7ce9c5c9015419b302631c6f70b62b
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-04-04 15:12:17 +0300

    Create app-routing.module.ts

---
+++ Angular/10_WorkshopPipesInterceptorsSubjects/workshop-interceptors/src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ErrorComponent } from './core/error/error.component';
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/home' },
+  { path: 'home', component: HomeComponent },
+  {
+    path: 'auth',
+    loadChildren: () => import('./user/user.module').then((m) => m.UserModule),
+  },
+  { path: 'error', component: ErrorComponent },
+  { path: '**', redirectTo: '/404' },
+  { path: '404', component: NotFoundComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
