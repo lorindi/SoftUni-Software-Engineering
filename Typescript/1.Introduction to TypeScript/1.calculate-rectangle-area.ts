e9164660f7a6343555697778cda8d4a8aa79e82a
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-04-05 10:34:04 +0300

    Create 1.calculate-rectangle-area.ts

---
+++ Typescript/1.Introduction to TypeScript/1.calculate-rectangle-area.ts
@@ -0,0 +1,6 @@
+const lengthRectangle: number = 5;
+const widthRectangle: number = 7;
+function calculateRectangleArea(lengthRectangle: number, widthRectangle: number): number {
+  return lengthRectangle * widthRectangle;
+}
+console.log(calculateRectangleArea(lengthRectangle, widthRectangle));
