9f1a3d0902b02a79ffae397b7dced4bd55a63b75
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-04-05 12:57:08 +0300

    Add express configuration
    Add handlebars configuration

--- JS-Back-End/4.Workshop ExpressJS and Templating/src/index.js
+++ JS-Back-End/4.Workshop ExpressJS and Templating/src/index.js
@@ -1,22 +1,14 @@
 const express = require("express");
-const handlebars = require("express-handlebars");
-const path = require('path')
+
+// require('./config/expressConfig')(app)
+const expressConfig = require("./config/expressConfig");
+const handlebarsConfig = require("./config/handlebarsConfig");
 
 const app = express();
 const PORT = 5000;
 
-//Express config
-app.use(express.static(path.resolve(__dirname, "public")));
-
-//Handlebars config
-app.engine(
-  "hbs",
-  handlebars.engine({
-    extname: "hbs",
-  })
-);
-app.set("view engine", "hbs");
-app.set("views", "src/views");
+expressConfig(app);
+handlebarsConfig(app);
 
 //Routes
 app.get("/", (req, res) => {
