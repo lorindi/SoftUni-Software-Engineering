4bf7016ddcd8e04ce73990cc183beb96f19d5ec6
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-04-05 11:50:28 +0300

    Add handlebars to project

--- JS-Back-End/4.Workshop ExpressJS and Templating/src/index.js
+++ JS-Back-End/4.Workshop ExpressJS and Templating/src/index.js
@@ -1,9 +1,18 @@
 const express = require("express");
+const handlebars = require('express-handlebars')
 
 const app = express();
 const PORT = 5000
 
+//Handlebars config
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs', 
+}))
+app.set('view engine', 'hbs')
+app.set('views', 'src/views')
+
+//Routes
 app.get('/', (req, res) => {
-    res.send('Hello from express')
+    res.render('index')
 })
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`))
