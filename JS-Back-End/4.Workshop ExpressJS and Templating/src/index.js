6e1d1eb9650197d53c511724247829c5abadb6bf
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-04-05 12:07:29 +0300

    Add static asset resolving

--- JS-Back-End/4.Workshop ExpressJS and Templating/src/index.js
+++ JS-Back-End/4.Workshop ExpressJS and Templating/src/index.js
@@ -1,18 +1,25 @@
 const express = require("express");
-const handlebars = require('express-handlebars')
+const handlebars = require("express-handlebars");
+const path = require('path')
 
 const app = express();
-const PORT = 5000
+const PORT = 5000;
+
+//Express config
+app.use(express.static(path.resolve(__dirname, "public")));
 
 //Handlebars config
-app.engine('hbs', handlebars.engine({
-    extname: 'hbs', 
-}))
-app.set('view engine', 'hbs')
-app.set('views', 'src/views')
+app.engine(
+  "hbs",
+  handlebars.engine({
+    extname: "hbs",
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", "src/views");
 
 //Routes
-app.get('/', (req, res) => {
-    res.render('index')
-})
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`))
+app.get("/", (req, res) => {
+  res.render("index");
+});
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
