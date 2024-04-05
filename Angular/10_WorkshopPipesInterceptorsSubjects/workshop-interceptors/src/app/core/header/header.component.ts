f21145f21a337aa8731456cd0be66ff74de994e8
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-04-05 10:42:52 +0300

    Create header.component.ts

---
+++ Angular/10_WorkshopPipesInterceptorsSubjects/workshop-interceptors/src/app/core/header/header.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { UserService } from 'src/app/user/user.service';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css'],
+})
+export class HeaderComponent {
+  constructor(private userService: UserService, private router: Router) {}
+
+  get isLoggedIn(): boolean {
+    return this.userService.isLogged;
+  }
+
+  get username(): string {
+    return this.userService.user?.username || '';
+  }
+
+  logout() {
+    this.userService.logout().subscribe({
+      next: () => {
+        this.router.navigate(['/auth/login']);
+      },
+      error: () => {
+        this.router.navigate(['/auth/login']);
+      },
+    });
+  }
+}
