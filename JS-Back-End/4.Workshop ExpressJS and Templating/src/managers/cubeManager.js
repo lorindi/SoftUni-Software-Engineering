73e2d961c51ade997bd430b23382afeb38a48761
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-04-05 23:57:23 +0300

    Add id generator

--- JS-Back-End/4.Workshop ExpressJS and Templating/src/managers/cubeManager.js
+++ JS-Back-End/4.Workshop ExpressJS and Templating/src/managers/cubeManager.js
@@ -1,9 +1,15 @@
+const uniqid = require('uniqid')
 const cubes = [];
 
 exports.getAll = () => cubes.slice();
 
 exports.create = (cubeData) => {
-  const newCube = { id: cubes.length + 1, ...cubeData };
+  const newCube = {
+    id: uniqid(),
+    // id: cubes.length + 1,
+    // id: (new Date()).getTime(),
+    ...cubeData,
+  };
   cubes.push(newCube);
   return cubeData;
 };
