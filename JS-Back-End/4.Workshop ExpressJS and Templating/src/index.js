ab990bfd8fe34bc608ce673243073613c3a1f4d9
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-04-05 11:33:48 +0300

    Add express project

--- JS-Back-End/4.Workshop ExpressJS and Templating/src/index.js
+++ JS-Back-End/4.Workshop ExpressJS and Templating/src/index.js
@@ -1,1 +1,9 @@
-console.log('Hello world');
+const express = require("express");
+
+const app = express();
+const PORT = 5000
+
+app.get('/', (req, res) => {
+    res.send('Hello from express')
+})
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`))
