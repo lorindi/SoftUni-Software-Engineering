eda5d1103f26ba33250ab1892eeef82eeec05fb1
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-04-06 23:23:30 +0300

    Create profile.component.ts

---
+++ Angular/10_WorkshopPipesInterceptorsSubjects/workshop-interceptors/src/app/user/profile/profile.component.ts
@@ -0,0 +1,67 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { EMAIL_DOMAINS } from 'src/app/constants';
+import { emailValidator } from 'src/app/shared/utils/email-validator';
+import { ProfileDetails } from 'src/app/types/user';
+import { UserService } from '../user.service';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css'],
+})
+export class ProfileComponent implements OnInit {
+  showEditMode: boolean = false;
+
+  profileDetails: ProfileDetails = {
+    username: '',
+    tel: '',
+    email: '',
+  };
+
+  form = this.fb.group({
+    username: ['', [Validators.required, Validators.minLength(5)]],
+    email: ['', [Validators.required, emailValidator(EMAIL_DOMAINS)]],
+    tel: [''],
+  });
+
+  constructor(private fb: FormBuilder, private userService: UserService) {}
+
+  ngOnInit(): void {
+    const { username, tel, email } = this.userService.user!;
+
+    this.profileDetails = {
+      username,
+      tel,
+      email,
+    };
+
+    this.form.setValue({
+      username,
+      tel,
+      email,
+    });
+  }
+
+  onToggle(): void {
+    this.showEditMode = !this.showEditMode;
+  }
+
+  saveProfileHandle(): void {
+    if (this.form.invalid) {
+      return;
+    }
+
+    this.profileDetails = this.form.value as ProfileDetails;
+    const { username, email, tel } = this.profileDetails;
+
+    this.userService.updateProfile(username, email, tel).subscribe(() => {
+      this.onToggle();
+    });
+  }
+
+  onCancel(e: Event) {
+    e.preventDefault();
+    this.onToggle();
+  }
+}
