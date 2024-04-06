47cb36fd32b325b45f1800f591cb313cdb295625
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-04-06 23:16:45 +0300

    Search from remembers

--- JS-Back-End/4.Workshop ExpressJS and Templating/src/controllers/homeController.js
+++ JS-Back-End/4.Workshop ExpressJS and Templating/src/controllers/homeController.js
@@ -8,7 +8,7 @@ router.get("/", (req, res) => {
   // req.body
   // req.query
   const cubes = cubeManager.getAll(search, from, to);
-  res.render("index", { cubes });
+  res.render("index", { cubes, search, from, to });
 });
 router.get("/about", (req, res) => {
   res.render("about");
