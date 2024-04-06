9acdd350b61c08e029469bb4e77ff7b1cb73c05c
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-04-06 23:23:38 +0300

    Create register.component.ts

---
+++ Angular/10_WorkshopPipesInterceptorsSubjects/workshop-interceptors/src/app/user/register/register.component.ts
@@ -0,0 +1,58 @@
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { EMAIL_DOMAINS } from 'src/app/constants';
+import { emailValidator } from 'src/app/shared/utils/email-validator';
+import { matchPasswordsValidator } from 'src/app/shared/utils/match-passwords-validator';
+import { UserService } from '../user.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css'],
+})
+export class RegisterComponent {
+  form = this.fb.group({
+    username: ['', [Validators.required, Validators.minLength(5)]],
+    email: ['', [Validators.required, emailValidator(EMAIL_DOMAINS)]],
+    tel: [''],
+    passGroup: this.fb.group(
+      {
+        password: ['', [Validators.required]],
+        rePassword: ['', [Validators.required]],
+      },
+      {
+        validators: [matchPasswordsValidator('password', 'rePassword')],
+      }
+    ),
+  });
+
+  get passGroup() {
+    return this.form.get('passGroup');
+  }
+
+  constructor(
+    private fb: FormBuilder,
+    private userService: UserService,
+    private router: Router
+  ) {}
+
+  register(): void {
+    if (this.form.invalid) {
+      return;
+    }
+
+    const {
+      username,
+      email,
+      tel,
+      passGroup: { password, rePassword } = {},
+    } = this.form.value;
+
+    this.userService
+      .register(username!, email!, tel!, password!, rePassword!)
+      .subscribe(() => {
+        this.router.navigate(['/themes']);
+      });
+  }
+}
