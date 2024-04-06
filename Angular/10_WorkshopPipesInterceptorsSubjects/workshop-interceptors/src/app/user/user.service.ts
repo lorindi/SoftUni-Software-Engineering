3a705dd7d2ed4555505cc34db76ae45087ba540f
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-04-06 23:23:45 +0300

    Create user.service.ts

---
+++ Angular/10_WorkshopPipesInterceptorsSubjects/workshop-interceptors/src/app/user/user.service.ts
@@ -0,0 +1,77 @@
+import { Injectable, OnDestroy } from '@angular/core';
+import { UserForAuth } from '../types/user';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Subscription, tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService implements OnDestroy {
+  private user$$ = new BehaviorSubject<UserForAuth | undefined>(undefined);
+  private user$ = this.user$$.asObservable();
+
+  user: UserForAuth | undefined;
+  USER_KEY = '[user]';
+
+  userSubscription: Subscription;
+
+  get isLogged(): boolean {
+    return !!this.user;
+  }
+
+  constructor(private http: HttpClient) {
+    this.userSubscription = this.user$.subscribe((user) => {
+      this.user = user;
+    });
+  }
+
+  login(email: string, password: string) {
+    return this.http
+      .post<UserForAuth>('/api/login', { email, password })
+      .pipe(tap((user) => this.user$$.next(user)));
+  }
+
+  register(
+    username: string,
+    email: string,
+    tel: string,
+    password: string,
+    rePassword: string
+  ) {
+    return this.http
+      .post<UserForAuth>('/api/register', {
+        username,
+        email,
+        tel,
+        password,
+        rePassword,
+      })
+      .pipe(tap((user) => this.user$$.next(user)));
+  }
+
+  logout() {
+    return this.http
+      .post('/api/logout', {})
+      .pipe(tap(() => this.user$$.next(undefined)));
+  }
+
+  getProfile() {
+    return this.http
+      .get<UserForAuth>('/api/users/profile')
+      .pipe(tap((user) => this.user$$.next(user)));
+  }
+
+  updateProfile(username: string, email: string, tel?: string) {
+    return this.http
+      .put<UserForAuth>('/api/users/profile', {
+        username,
+        email,
+        tel,
+      })
+      .pipe(tap((user) => this.user$$.next(user)));
+  }
+
+  ngOnDestroy(): void {
+    this.userSubscription.unsubscribe();
+  }
+}
