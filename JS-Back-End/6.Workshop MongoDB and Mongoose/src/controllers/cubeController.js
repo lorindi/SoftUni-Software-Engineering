9705752161c9ea0fa121489e9d30c9c978f15a92
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-04-08 22:39:52 +0300

    Update cubeController.js

--- JS-Back-End/6.Workshop MongoDB and Mongoose/src/controllers/cubeController.js
+++ JS-Back-End/6.Workshop MongoDB and Mongoose/src/controllers/cubeController.js
@@ -27,7 +27,7 @@ router.get("/:cubeId/details", async (req, res) => {
 router.get("/:cubeId/attach-accessory", async (req, res) => {
   const cube = await cubeManager.getOne(req.params.cubeId).lean();
   const accessories = await accessoryManager.getAll().lean();
-
-  res.render("accessory/attach", { cube, accessories });
+  const hasAccessories = accessories.length > 0;
+  res.render("accessory/attach", { cube, accessories, hasAccessories  });
 });
 module.exports = router;
