12ea9e680ba3e8d7e329ce5dfb5913ab69ed2895
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-04-09 13:55:00 +0300

    Create towns.ts

---
+++ Typescript/2.Advanced Data Types and Interfaces/src/problem-solving/towns.ts
@@ -0,0 +1,17 @@
+// Input:
+//  ["Sofia | 42.696552 | 23.32601", "Beijing | 39.913818 | 116.363625"];
+
+// Output:
+//      [{ town: 'Sofia', latitude: '42.70', longitude: '23.33' }
+//      { town: 'Beijing', latitude: '39.91', longitude: '116.36'}]
+
+type Town = {
+  town: string;
+  latitude: string;
+  longtitute: string;
+};
+
+const solve = (tableRows: string[]): Town[] => {
+  //
+  return [];
+};
