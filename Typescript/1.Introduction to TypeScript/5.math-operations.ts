fd998807ecd5c012916f3325aa4f2e99e93f4008
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-04-09 13:54:08 +0300

    Create 5.math-operations.ts

---
+++ Typescript/1.Introduction to TypeScript/5.math-operations.ts
@@ -0,0 +1,29 @@
+function matchOperations(
+    firstNumber: number,
+    secondNumber: number,
+    sign: string
+  ): void {
+    const operations: { [key: string]: () => number } = {
+      "+": () => {
+        return firstNumber + secondNumber;
+      },
+      "-": () => {
+        return firstNumber - secondNumber;
+      },
+      "*": () => {
+        return firstNumber * secondNumber;
+      },
+      "/": () => {
+        return firstNumber / secondNumber;
+      },
+    };
+  
+    if (sign in operations) {
+      console.log(operations[sign]());
+    } else {
+      console.log("Invalid operation");
+    }
+  }
+  
+  matchOperations(3, 5.5, '*');
+  
