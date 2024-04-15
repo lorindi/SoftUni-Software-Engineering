4bab873324765d56e403f82a32f01e6f6b423d5b
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-04-15 11:02:28 +0300

    Update index.js

--- JS-Back-End/7.Session and Authentication/index.js
+++ JS-Back-End/7.Session and Authentication/index.js
@@ -1,18 +1,34 @@
 express = require("express");
 const cookieParser = require("cookie-parser");
+const expressSession = require("express-session");
 const { v4: uuid } = require("uuid");
 
 const app = express();
+const session = {};
+
 app.use(cookieParser());
+app.use(
+  expressSession({
+    secret: "my secret",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
 
 app.get("/", (req, res) => {
   let id;
-
   const userId = req.cookies["userId"];
-  if (userId) {
+  if (userId && session[userId]) {
     id = userId;
+    // console.log("User secret: ", session[userId].secret);
+    console.log(req.session.secret);
   } else {
     id = uuid();
+    // session[id] = {
+    //   secret: "my secret",
+    // };
+    req.session.secret = ` ${id} - some secret`;
     res.cookie("userId", id);
   }
 
