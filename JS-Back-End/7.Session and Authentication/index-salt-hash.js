8456c52cd0fef51cdf6e9e6e762354c91d5ff9cb
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-04-16 16:54:06 +0300

    Create index-salt-hash.js

---
+++ JS-Back-End/7.Session and Authentication/index-salt-hash.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const { v4: uuid } = require("uuid");
+const bcrypt = require("bcrypt");
+
+const app = express();
+const session = {};
+
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
+let salt = "";
+
+const users = {};
+
+app.get("/", (req, res) => {
+  console.log(users);
+  res.send("OK");
+});
+app.get("/register", (req, res) => {
+  res.send(`
+      <form method="POST">
+          <label for="username">Username</label>
+          <input type="text" name="username"/>
+          <label for="password">Password</label>
+          <input type="password" name="password"/>
+          <input type="submit" value="Register"/>
+      </form>
+      `);
+});
+
+app.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+
+  if (!salt) {
+    salt = await bcrypt.genSalt(10);
+  }
+  const hash = await bcrypt.hash(password, salt);
+  users[username] = {
+    password: hash,
+  };
+  res.redirect("/login");
+});
+
+app.get("/login", (req, res) => {
+  res.send(`
+    <form method="POST">
+        <label for="username">Username</label>
+        <input type="text" name="username"/>
+        <label for="password">Password</label>
+        <input type="password" name="password"/>
+        <input type="submit" value="Login"/>
+    </form>
+    `);
+});
+
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const hash = users[username]?.password;
+  const isValid = await bcrypt.compare(password, hash);
+  if (isValid) {
+    res.send("Successfully logged in");
+  } else {
+    res.send("Unauthorized");
+  }
+});
+
+app.listen(5000, () => console.log("Server is listening port 5000..."));
