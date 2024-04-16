03254fa5e1938036bbfe6de606ac1fce29032986
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-04-16 16:19:54 +0300

    Create index-session.js

---
+++ JS-Back-End/7.Session and Authentication/index-session.js
@@ -0,0 +1,38 @@
+express = require("express");
+const cookieParser = require("cookie-parser");
+const expressSession = require("express-session");
+const { v4: uuid } = require("uuid");
+
+const app = express();
+const session = {};
+
+app.use(cookieParser());
+app.use(
+  expressSession({
+    secret: "my secret",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
+
+app.get("/", (req, res) => {
+  let id;
+  const userId = req.cookies["userId"];
+  if (userId && session[userId]) {
+    id = userId;
+    // console.log("User secret: ", session[userId].secret);
+    console.log(req.session.secret);
+  } else {
+    id = uuid();
+    // session[id] = {
+    //   secret: "my secret",
+    // };
+    req.session.secret = ` ${id} - some secret`;
+    res.cookie("userId", id, { httpOnly: true });
+  }
+
+  res.send(`Hello User - ${id}`);
+});
+
+app.listen(5000, () => console.log("Server is listening port 5000..."));
