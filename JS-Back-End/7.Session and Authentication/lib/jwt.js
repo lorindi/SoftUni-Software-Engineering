4c8d5a21e2b84286ab7b5ccee3514d5387efc981
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-04-16 18:32:08 +0300

    Update jwt.js

--- JS-Back-End/7.Session and Authentication/lib/jwt.js
+++ JS-Back-End/7.Session and Authentication/lib/jwt.js
@@ -1,31 +1,7 @@
+const util = require("util");
 const jwt = require("jsonwebtoken");
-
-const sign = (payload, secret, options) => {
-  const promise = new Promise((resolve, reject) => {
-    jwt.sign(payload, secret, options, (err, result) => {
-      if (err) {
-        return reject(err);
-      }
-      resolve(result);
-    });
-  });
-  return promise;
-};
-
-const verify = (token, secret) => {
-  const promise = new Promise((resolve, reject) => {
-    jwt.sign(token, secret, (err, result) => {
-      if (err) {
-        return reject(err);
-      }
-      resolve(result);
-    });
-  });
-  return promise;
-};
-
 const jwtPromises = {
-  sign,
-  verify,
+  sign: util.promisify(jwt.sign),
+  verify: util.promisify(jwt.verify),
 };
 module.exports = jwtPromises;
