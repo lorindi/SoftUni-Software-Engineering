77181a419953d84800163b86a94540b2b9bd9280
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-04-16 22:02:19 +0300

    Update userController.js

--- JS-Back-End/8.Workshop Session and Authentication/src/controllers/userController.js
+++ JS-Back-End/8.Workshop Session and Authentication/src/controllers/userController.js
@@ -2,4 +2,9 @@ const router = require("express").Router();
 router.get("/register", (req, res) => {
   res.render("users/register");
 });
+router.post("/register", (req, res) => {
+  const { username, password, repeatPassword } = req.body;
+  console.log(req.body);
+  res.redirect("/users/login");
+});
 module.exports = router;
