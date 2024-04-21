7bc96ced2399b818ba92f57feb8b211e5d2c6bb2
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-04-21 10:04:47 +0300

    Create index.js

---
+++ JS-Back-End/Petstagram/src/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const handlebars = require("express-handlebars");
+const path = require("path");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const routes = require("./routes");
+
+const app = express();
+
+mongoose
+  .connect(`mongodb://127.0.0.1:27017/petstagram`)
+  .then(() => {
+    console.log("DB Connected successfully");
+  })
+  .catch((err) => console.log(`Db Error`, err));
+
+app.engine(
+  "hbs",
+  handlebars.engine({
+    extname: "hbs",
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", "src/views");
+
+app.use(express.static(path.resolve(__dirname, "public")));
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(routes);
+
+app.listen(5000, console.log(`Server is listening on port 5000...`));
