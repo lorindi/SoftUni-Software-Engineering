fdde6c4ffca30d330e6efd62555428f7b3a013b3
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-04-21 20:16:04 +0300

    Update routes.js

--- JS-Back-End/Petstagram/src/routes.js
+++ JS-Back-End/Petstagram/src/routes.js
@@ -3,4 +3,7 @@ const homeController = require("./controllers/homeController");
 const userController = require("./controllers/userController");
 router.use(homeController);
 router.use("/users", userController);
+router.get("*", (req, res) => {
+  res.redirect('/404');
+});
 module.exports = router;
