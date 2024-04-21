b8fb04102b19f00abc364bf62428edb42a4cc626
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-04-21 20:55:44 +0300

    Update userController.js

--- JS-Back-End/Petstagram/src/controllers/userController.js
+++ JS-Back-End/Petstagram/src/controllers/userController.js
@@ -1,16 +1,25 @@
 const router = require("express").Router();
 const userManager = require("../managers/userManager");
-const {TOKEN_KEY } = require("../config/config");
+const { TOKEN_KEY } = require("../config/config");
+const { getErrorMessage } = require("../utils/errorHelpers");
 
 router.get("/login", (req, res) => {
   res.render("users/login");
 });
 
-router.post("/login", async (req, res) => {
+router.post("/login", async (req, res, 
+  // next
+) => {
   const { username, password } = req.body;
-  const token = await userManager.login(username, password);
-  res.cookie(TOKEN_KEY, token);
-  res.redirect("/");
+
+  try {
+    const token = await userManager.login(username, password);
+    res.cookie(TOKEN_KEY, token);
+    res.redirect("/");
+  } catch (err) {
+    res.render("users/login", { error: getErrorMessage(err) });
+    // next(err)
+  }
 });
 
 router.get("/register", (req, res) => {
@@ -19,8 +28,13 @@ router.get("/register", (req, res) => {
 
 router.post("/register", async (req, res) => {
   const { username, email, password, repeatPassword } = req.body;
-  await userManager.register({ username, email, password, repeatPassword });
-  res.redirect("/users/login");
+  try {
+    const token = await userManager.register({ username, email, password, repeatPassword });
+    res.cookie(TOKEN_KEY, token)
+    res.redirect("/");
+  } catch (err) {
+    res.render("users/register", { error: getErrorMessage(err) });
+  }
 });
 
 router.get("/logout", (req, res) => {
