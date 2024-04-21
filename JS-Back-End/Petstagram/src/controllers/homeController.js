781fa52cecae326d53167255a560470a5a217a4f
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-04-21 20:15:47 +0300

    Update homeController.js

--- JS-Back-End/Petstagram/src/controllers/homeController.js
+++ JS-Back-End/Petstagram/src/controllers/homeController.js
@@ -1,5 +1,10 @@
 const router = require("express").Router();
 router.get("/", (req, res) => {
+  // console.log(req.user);
     res.render('home')
   });
+
+router.get('/404', (req, res) => {
+  res.render('404')
+})
 module.exports = router;
