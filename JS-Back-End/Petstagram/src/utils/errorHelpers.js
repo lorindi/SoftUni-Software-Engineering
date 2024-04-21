c731ddac6ad4bdb4e12db4adfe105e935ecbb3bf
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-04-21 20:56:02 +0300

    Update errorHelpers.js

--- JS-Back-End/Petstagram/src/utils/errorHelpers.js
+++ JS-Back-End/Petstagram/src/utils/errorHelpers.js
@@ -3,6 +3,9 @@ const { MongooseError } = require("mongoose");
 exports.getErrorMessage = (err) => {
   if (err instanceof MongooseError) {
     return Object.values(err.errors).at(0).message;
+  } else if (err instanceof ValidationError) {
+    console.log(err);
+    return err.message;
   } else {
     return err.message;
   }
