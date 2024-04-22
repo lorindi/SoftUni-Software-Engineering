44e6057c2356c2285cf645e88c7eb6bd86cfc57f
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-04-22 14:50:59 +0300

    Create modul-a.ts

---
+++ Typescript/5.Namespaces and Modules/modules-demo/src/modul-a.ts
@@ -0,0 +1,19 @@
+// export const a = 5;
+
+// export function test() {
+//   return "test123";
+// }
+
+// export const test2 = () => "test12345";
+
+const a = 5;
+
+function test() {
+  return "test123";
+}
+
+export { a, test as getSomeRandomString };
+
+// export default function test() {
+//   return "test123";
+// }
