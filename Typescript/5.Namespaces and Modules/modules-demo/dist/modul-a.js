e35088249f3487807afc3916d79bdd96ce99beaf
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-04-22 14:50:46 +0300

    Create modul-a.js

---
+++ Typescript/5.Namespaces and Modules/modules-demo/dist/modul-a.js
@@ -0,0 +1,17 @@
+"use strict";
+// export const a = 5;
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getSomeRandomString = exports.a = void 0;
+// export function test() {
+//   return "test123";
+// }
+// export const test2 = () => "test12345";
+const a = 5;
+exports.a = a;
+function test() {
+    return "test123";
+}
+exports.getSomeRandomString = test;
+// export default function test() {
+//   return "test123";
+// }
