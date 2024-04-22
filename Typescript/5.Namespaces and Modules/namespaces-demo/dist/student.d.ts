d214c3350a1c064b4a41f7fe54e66104d8f0e624
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-04-22 14:51:40 +0300

    Create student.d.ts

---
+++ Typescript/5.Namespaces and Modules/namespaces-demo/dist/student.d.ts
@@ -0,0 +1,8 @@
+declare namespace StudentGroup {
+    class Student {
+        marks: number[];
+        favouriteSubject: string;
+        constructor(marks: number[], favouriteSubject: string);
+    }
+}
+//# sourceMappingURL=student.d.ts.map
