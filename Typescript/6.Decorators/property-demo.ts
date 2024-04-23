71ec139749cfa697d2979b9e90373aba31e6f744
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-04-23 11:35:16 +0300

    Create property-demo.ts

---
+++ Typescript/6.Decorators/property-demo.ts
@@ -0,0 +1,31 @@
+// type Task = {
+//   name: string;
+//   level: "low" | "medium" | "hard";
+// };
+
+// class Employee {
+//   @withMoreTasks
+//   tasks: Task[] = [];
+// }
+
+// const employee1 = new Employee();
+// console.log(employee1);
+
+// const employee2 = new Employee();
+// console.log(employee2);
+
+// // Decorator scope
+// function withMoreTasks<T, V extends Task[]>(
+//   target: undefined,
+//   context: ClassFieldDecoratorContext
+// ) {
+//   // Property scope
+//   return function (fieldValue: V) {
+//     // API call -> async
+//     fieldValue.push({ name: "Do your homework!", level: "medium" });
+//     fieldValue.push({ name: "Go to work!", level: "low" });
+//     fieldValue.push({ name: "Go to the GYM!", level: "hard" });
+
+//     return fieldValue;
+//   };
+// }
