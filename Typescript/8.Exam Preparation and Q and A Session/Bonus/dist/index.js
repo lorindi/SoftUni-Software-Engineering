bc8ff244e80bd5707825878ed63eb10f6225944d
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-04-27 19:30:13 +0300

    Create index.js

---
+++ Typescript/8.Exam Preparation and Q and A Session/Bonus/dist/index.js
@@ -0,0 +1,20 @@
+const sortedNums = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
+];
+function binarySearch(sortNums, target, startIndex = 0, endIndex = sortNums.length - 1) {
+    if (endIndex < startIndex)
+        return -1;
+    const middleIndex = Math.floor((startIndex + endIndex) / 2);
+    const currentEl = sortNums[middleIndex];
+    // to right
+    if (currentEl < target) {
+        return binarySearch(sortNums, target, middleIndex + 1, endIndex);
+    }
+    // to left
+    if (currentEl > target) {
+        return binarySearch(sortNums, target, startIndex, middleIndex - 1);
+    }
+    return middleIndex;
+}
+console.log(binarySearch(sortedNums, 13));
+//# sourceMappingURL=index.js.map
