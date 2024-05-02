1383cc3817274d2428748838cb97ec7749dc905e
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-05-02 17:20:12 +0300

    Update routes.js

--- JS-Back-End/10.Exam Preparation/Petstagram/src/routes.js
+++ JS-Back-End/10.Exam Preparation/Petstagram/src/routes.js
@@ -1,9 +1,12 @@
 const router = require("express").Router();
 const homeController = require("./controllers/homeController");
 const userController = require("./controllers/userController");
+const photoController = require("./controllers/photoController");
+
 router.use(homeController);
 router.use("/users", userController);
+router.use("/photos", photoController);
 router.get("*", (req, res) => {
-  res.redirect('/404');
+  res.redirect("/404");
 });
 module.exports = router;
