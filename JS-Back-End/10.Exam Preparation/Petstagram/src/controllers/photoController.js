5ebe056927ccad6dc6c56d0f513d9097aff051e8
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-05-03 10:29:37 +0300

    Update photoController.js

--- JS-Back-End/10.Exam Preparation/Petstagram/src/controllers/photoController.js
+++ JS-Back-End/10.Exam Preparation/Petstagram/src/controllers/photoController.js
@@ -2,14 +2,19 @@ const router = require("express").Router();
 const photoManager = require("../managers/photoManager");
 const { getErrorMessage } = require("../utils/errorHelpers");
 
+router.get("/", async (req, res) => {
+  const photos = await photoManager.getAll().lean()
+  res.render("photos", { photos });
+});
+
 router.get("/create", (req, res) => {
   res.render("photos/create");
 });
 
 router.post("/create", async (req, res) => {
-  const photoData = { 
-    ...req.body, 
-    owner: req.user._id 
+  const photoData = {
+    ...req.body,
+    owner: req.user._id,
   };
 
   try {
