c73222ed6559a8c8d002cdaaf56421266225b0da
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-05-03 12:54:09 +0300

    Update photoManager.js

--- JS-Back-End/10.Exam Preparation/Petstagram/src/managers/photoManager.js
+++ JS-Back-End/10.Exam Preparation/Petstagram/src/managers/photoManager.js
@@ -1,5 +1,8 @@
 const Photo = require("../models/Photo");
 
-exports.getAll = () => Photo.find();
+exports.getAll = () => Photo.find().populate("owner");
+
+exports.getOne = (photoId) => Photo.findById(photoId).populate("owner");
+
 
 exports.create = (photoData) => Photo.create(photoData);
