84709131d0aac474493682303a1412b262f06e4f
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-05-03 09:48:58 +0300

    Update errorHandlerMiddleware.js

--- JS-Back-End/10.Exam Preparation/Petstagram/src/middlewares/errorHandlerMiddleware.js
+++ JS-Back-End/10.Exam Preparation/Petstagram/src/middlewares/errorHandlerMiddleware.js
@@ -1,5 +1,5 @@
 const { getErrorMessage } = require("../utils/errorHelpers");
 
-exports.errorHandler = (err, req, res) => {
-  res.render("/404", { error: getErrorMessage(err) });
+exports.errorHandler = (err, req, res, next) => {
+  res.render("404", { error: getErrorMessage(err) });
 };
