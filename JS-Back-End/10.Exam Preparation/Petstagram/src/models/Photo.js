b78698005f29efee46a20e954a3f292693ed3a0d
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-05-05 21:20:13 +0300

    Update Photo.js

--- JS-Back-End/10.Exam Preparation/Petstagram/src/models/Photo.js
+++ JS-Back-End/10.Exam Preparation/Petstagram/src/models/Photo.js
@@ -24,8 +24,21 @@ const photoSchema = new mongoose.Schema({
   },
   owner: {
     type: mongoose.Types.ObjectId,
-    ref: 'User',
+    ref: "User",
   },
+  comments: [
+    {
+      user: {
+        type: mongoose.Types.ObjectId,
+        required: [true],
+        ref: "User",
+      },
+      message: {
+        type: String,
+        required: [true, "Comment message is required"],
+      },
+    },
+  ],
 });
 const Photo = mongoose.model("Photo", photoSchema);
-module.exports = Photo
+module.exports = Photo;
