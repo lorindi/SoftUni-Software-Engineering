999c7091a6260053fd486da1625a27b78089bd58
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-05-05 22:06:10 +0300

    Update homeController.js

--- JS-Back-End/10.Exam Preparation/Petstagram/src/controllers/homeController.js
+++ JS-Back-End/10.Exam Preparation/Petstagram/src/controllers/homeController.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const photoManager = require("../managers/photoManager");
+const { isAuth } = require("../middlewares/authMiddleware");
 router.get("/", (req, res) => {
   // console.log(req.user);
   res.render("home");
@@ -9,8 +10,8 @@ router.get("/404", (req, res) => {
   res.render("404");
 });
 
-router.get("/profile", async (req, res) => {
+router.get("/profile", isAuth, async (req, res) => {
   const photos = await photoManager.getByOwner(req.user._id).lean();
-  res.render("profile",{photos, photoCount: photos.length });
+  res.render("profile", { photos, photoCount: photos.length });
 });
 module.exports = router;
