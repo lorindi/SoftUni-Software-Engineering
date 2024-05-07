765601d9fcdf3a89a54a8ded91cd23159ee8706f
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-05-07 22:01:35 +0300

    Create User.js

---
+++ JS-Back-End/11.Workshop REST API/project/server/models/User.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+userSchema.pre("save", async function () {
+  const hash = await bcrypt.hash(this.password, 10);
+  this.password = hash;
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
