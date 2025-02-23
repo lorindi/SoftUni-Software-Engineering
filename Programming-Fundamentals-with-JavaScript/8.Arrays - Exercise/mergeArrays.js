8691a4b967556cbb524c6d983af9b521e7e8d60f
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2025-02-23 13:50:52 +0200

    Update mergeArrays.js

--- Programming-Fundamentals-with-JavaScript/8.Arrays - Exercise/mergeArrays.js
+++ Programming-Fundamentals-with-JavaScript/8.Arrays - Exercise/mergeArrays.js
@@ -1,11 +1,10 @@
 function task(list1, list2) {
   let newList = [];
   for (let i = 0; i < list1.length; i++) {
-    if (i % 2 === 0) {
-      newList.push(Number(list1[i]) + Number(list2[i]));
-    } else {
-        newList.push(list1[i] + list2[i])
-    }
+    newList.push(i % 2 === 0 
+        ? Number(list1[i]) + Number(list2[i]) 
+        : list1[i] + list2[i]
+      );
   }
   console.log( newList.join(' - '));
   
