0ba5586aebcc06a2b60ed4dc1d7cd1c2eeb891f5
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2025-02-24 19:17:38 +0200

    Update maxNumber.js

--- Programming-Fundamentals-with-JavaScript/8.Arrays - Exercise/maxNumber.js
+++ Programming-Fundamentals-with-JavaScript/8.Arrays - Exercise/maxNumber.js
@@ -1,13 +1,28 @@
 function task(list) {
-  let bigger = [];
-  let number = 0;
-  let reverseList = list.reverse()
-  for (let num of reverseList) {
-    number = num > number ? num : number;
-    if (!bigger.includes(number)) {
-      bigger.push(number);
+  let sequence = 1;
+  let maxSequence = 0;
+  let maxSequenceNum = 0;
+  let listLength = list.length;
+
+  for (let i = 0; i < listLength; i++) {
+    if (i !== 0) {
+      let currentNumber = Number(list[i]);
+      let prevNumber = Number(list[i - 1]);
+
+      if (currentNumber === prevNumber) {
+        sequence++;
+
+        if (sequence > maxSequence) {
+          maxSequence = sequence;
+          maxSequenceNum = currentNumber;
+        }
+      } else {
+        sequence = 1;
+      }
     }
   }
-  console.log(bigger.reverse().join(' '));
+  console.log(`${(maxSequenceNum + ' ').repeat(maxSequence)}`);
+  
 }
-task([14, 24, 3, 19, 15, 17]);
+task([2, 1, 1, 2, 3, 3, 2, 2, 2, 1]
+);
